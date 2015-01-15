266f455be394a198cf75c10a135c3ea26fae781d
Kenneth Suralta <ksuralta@gmail.com>
2015-01-16 00:04:09 +1100

    Add jar files, update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #/bin/sh
-javac -cp src/main/java:lib/log4j-core-2.1.jar:lib/log4j-api-2.1.jar -proc:none src/main/java/*.java
+find ./src/main/java -name "*.java" > sources.txt
+javac -cp src/main/java:lib/log4j-core-2.1.jar:lib/log4j-api-2.1.jar -d bin -proc:none @sources.txt
