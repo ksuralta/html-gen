f811b01a2c84b8757e122a942dd006e42dba6f7f
Kenneth Suralta <ksuralta@gmail.com>
2015-01-16 00:15:24 +1100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #/bin/sh
 find ./src/main/java -name "*.java" > sources.txt
+rm -rf bin && mkdir bin
 javac -cp src/main/java:lib/log4j-core-2.1.jar:lib/log4j-api-2.1.jar -d bin -proc:none @sources.txt
